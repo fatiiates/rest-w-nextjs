1bc0316c159086adb7b80adba88ee0ff02c93d35
fatiiates <fatiiates@gmail.com>
2020-12-07 00:29:25 +0300

    0.1.0

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Box from '@material-ui/core/Box';
+
+
+const Default = (): React.ReactNode => {
+
+  return (
+    <Box>
+a   
+    </Box>
+  );
+
+}
+
+export default Default
